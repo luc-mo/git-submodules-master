0e0e975dfb33ed757db6dcf0c7aa8835fdaaf452
SnowDrive <luc.moralesr@gmail.com>
2025-07-08 07:53:52 -0400

    feat: integrate plugin to resolve path aliases from submodule lib

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,26 @@
 import { defineConfig } from 'tsup'
+import fs from 'node:fs'
+import path from 'node:path'
+import type { Plugin } from 'esbuild'
+
+const libAliasesPlugin: Plugin = {
+  name: 'lib-aliases',
+  setup(build) {
+    build.onResolve({ filter: /^@\// }, (args) => {
+			const isLib =
+				args.importer.includes('lib/') ||
+				args.importer.includes('lib\\')
+
+			if (isLib) {
+        const resolvedPath = path.resolve('./lib/src/', args.path.replace('@/', ''))
+        const fullPath = resolvedPath.concat('.ts')
+				const fileExists = fs.existsSync(fullPath)
+				if(fileExists) return { path: fullPath }
+        return { path: resolvedPath }
+      }
+    })
+  },
+}
 
 export default defineConfig({
 	entry: ['src/index.ts'],
@@ -7,4 +29,5 @@ export default defineConfig({
 	clean: true,
 	treeshake: false,
 	minify: false,
+	esbuildPlugins: [libAliasesPlugin],
 })
