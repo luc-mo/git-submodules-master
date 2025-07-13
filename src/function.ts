e2048701940d7a80c19938f451c8d49099324309
SnowDrive <luc.moralesr@gmail.com>
2025-07-13 17:44:41 -0400

    feat: update imports and function calls

---
+++ src/function.ts
@@ -0,0 +1,4 @@
+export const internalMasterFunction = () => {
+  console.log('Calling internal function from "master"')
+  return 'This is an internal "master" function'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-import { mainFile } from '@lib'
-import { main } from '@sub-lib'
-import { file } from '@/file'
+import { internalLibFunction, externalLibFunction } from '@lib'
+import { axiosCall } from '@sub-lib'
+import { internalMasterFunction } from '@/function'
 
-mainFile()
+const main = async() => {
+  const internalLibCall = internalLibFunction()
+  console.log({ internalLibCall })
+
+  const externalLibCall = await externalLibFunction()
+  console.log({ externalLibCall })
+
+  const subLibAxiosCall = await axiosCall()
+  console.log({ subLibAxiosCall })
+
+  const internalMasterCall = internalMasterFunction()
+  console.log({ internalMasterCall })
+}
+
 main()
-console.log(file)
